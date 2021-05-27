8a1050ae9eb67736749cde44f01c4bc2f441f2bd
VinodPawar4643 <pawarvinodvp1@gmail.com>
2021-05-27 16:35:04 +0530

    Add project files.

---
+++ CSharpConsoleNew/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsoleNew
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region non static region
+			//dataTypes d = new dataTypes();
+			//d.name = "Vinod ";
+			//d.number = 4643;
+			//Console.WriteLine("Non static name is : "+d.name); // call non static varibles 
+			//Console.WriteLine("Non static number is : " + d.number);
+			//d.callNonSaticMethod(); // call non static methods 
+			#endregion
+
+			#region
+			//dataTypes.staicVariable = 1000;
+			//Console.WriteLine("StaticVariable is : "+dataTypes.staicVariable); // call static variable
+			//dataTypes.calllStaticMethod(); // call static method
+			#endregion
+
+		}
+	}
+}
---
+++ CSharpConsoleNew/dataTypes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsoleNew
+{
+	class dataTypes
+	{
+    public int number = 0;
+    public static int staicVariable = 0;
+    public string name = "";
+
+    public static void calllStaticMethod()
+    {
+      Console.WriteLine("Calling static method :" + staicVariable);
+      staicVariable = 9000;
+    }
+
+    public void callNonSaticMethod()
+    {
+      Console.WriteLine("Enter any numeric value : ");
+      number = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine(number);
+      Console.WriteLine("Enter any name : ");
+      name = Console.ReadLine();
+    }
+  }
+}
